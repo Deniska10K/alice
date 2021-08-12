ea24d71d7b3ab09a025e5ec216c1249bdbea2f5c
Deniska10K <sorokin20072003@gmail.com>
2021-08-12 19:12:19 +0300

    Deploy

--- module13/task_1.py
+++ module13/task_1.py
@@ -28,11 +28,11 @@ def float_converter(n):
     k = 0
     if n < 1:
         while n < 1:
-            n *= 10
+            n = round(n * 10, len(str(n)))
             k -= 1
     elif n >= 10:
         while n >= 10:
-            n /= 10
+            n = round(n / 10, len(str(n)))
             k += 1
     return f"Формат плавающей точки: x = {n} * 10 ** {k}"
 
